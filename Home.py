75258b03a85caa8212d3936521bec9895143c42b
Anshsahu <160406639+AnshSahu11@users.noreply.github.com>
2025-08-10 18:43:30 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -79,9 +79,9 @@ and ensure you make smart, data-driven decisions.
 📍 Focused exclusively on **Gurgaon**, this tool brings **local expertise with global-level analytics** to your fingertips.
 
  
-    ### 📚 Acknowledgements
-    - [99acres.com](https://www.99acres.com): Source of property listing data.  
-    - [CampusX DSMP](https://learnwith.campusx.in/t/u/activeCourses): Inspiration from their capstone structure.
+### 📚 Acknowledgements
+- [99acres.com](https://www.99acres.com): Source of property listing data.  
+- [CampusX DSMP](https://learnwith.campusx.in/t/u/activeCourses): Inspiration from their capstone structure.
     
 """)
 
